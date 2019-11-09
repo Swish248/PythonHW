0acad66e26a1d2d11592bad89f35ef7e7234a6d9
Swish248 <dheethya.balaji@gmail.com>
2019-11-09 13:32:17 -0500

    Rotate Left Warmup 11/9/19

---
+++ rotateleftwarmup.py
@@ -0,0 +1,12 @@
+#DheethyaB_RotateLeftwarmupc#120192020
+#c#1
+#jndkndkjfn
+#Written by Dheethya_Balaji
+def rotateleft(a, b, c):
+    list1 = [a, b, c]
+    x = (list1.pop(0))
+    list1.append(x)
+
+    return list1
+
+print(rotateleft(1, 2, 3))
